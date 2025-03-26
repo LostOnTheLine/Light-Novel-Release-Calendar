acb9ae39b05dee56b217c5ae632445da5aad3362
LostOnTheLine <60684934+LostOnTheLine@users.noreply.github.com>
2025-03-25 23:34:33 -0700

    Update update_calendar.py

--- calendar/update_calendar.py
+++ calendar/update_calendar.py
@@ -1,15 +1,20 @@
 import json
 import os
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
+from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from dateutil.parser import parse
 from datetime import datetime
 
 JSON_FILE = "light_novel_releases.json"
 CALENDAR_ID = os.getenv("CALENDAR_ID")
+SCOPES = ["https://www.googleapis.com/auth/calendar"]
 
-creds = Credentials.from_authorized_user_info(json.loads(os.getenv("GOOGLE_CREDENTIALS")))
+# Load Service Account credentials from GOOGLE_CREDENTIALS environment variable
+creds = service_account.Credentials.from_service_account_info(
+    json.loads(os.getenv("GOOGLE_CREDENTIALS")),
+    scopes=SCOPES
+)
+
 service = build("calendar", "v3", credentials=creds)
 
 with open(JSON_FILE, "r") as f:
